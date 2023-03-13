b47eebff528a3ddb9a51da53010bea4a56139e0b
NovaSagittarii <novasagittarii@gmail.com>
2023-03-12 18:38:21 -0600

    scroll-animated circles slide-in-from-right (jank)

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if(entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+const leadPhotos = document.querySelector('#lead-photos');
+const svgs = [...new Array(11)].map(() => 'circle.svg');
+const leadImages = svgs.map((src) => {
+    const img = document.createElement('img');
+    img.classList.add('circle', 'show');
+    img.src = src;
+    img.alt = "circle";
+    leadPhotos.append(img);
+    return img;
+});
+document.addEventListener('scroll', () => {
+    for(const img of leadImages){
+        const boundingClientRect = img.getBoundingClientRect();
+        const stagger = Math.max(boundingClientRect.height, boundingClientRect.width) * (boundingClientRect.x / document.body.getBoundingClientRect().width);
+        const y = boundingClientRect.y + stagger;
+        const offset = Math.max(y-window.innerHeight, 0) > 0 ? 2000 : 0;
+        img.style.transform = `translateX(${offset}px)`;
+    }
+});
